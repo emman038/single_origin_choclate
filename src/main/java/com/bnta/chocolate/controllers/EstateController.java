df2b9f2fe8e230971010c186042d110b5b08af24
arianna coseschi <ariannacoseschi@gmail.com>
2023-11-06 18:17:29 +0000

    MVP completed

---
+++ src/main/java/com/bnta/chocolate/controllers/EstateController.java
@@ -0,0 +1,31 @@
+package com.bnta.chocolate.controllers;
+
+
+import com.bnta.chocolate.models.Estate;
+import com.bnta.chocolate.services.EstateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/estates")
+public class EstateController {
+
+    @Autowired
+    EstateService estateService ;
+
+    //index route
+
+
+    @GetMapping
+    public ResponseEntity<List<Estate>> getAllEstates(){
+        List<Estate> estates  = estateService.getAllEstates();
+        return new ResponseEntity<>(estates, HttpStatus.OK);
+    }
+
+}
